ea0f9accdd4557d5afc9c36f557428bd1a87c85e
j4sysiak <j4sysiak@gmail.com>
2019-03-17 07:30:25 +0100

    1

---
+++ spring-tutorial-Aspect-Oriented-Programming-AOP-88/src/com/jaceksysiak/spring/aop/App.java
@@ -0,0 +1,73 @@
+package com.jaceksysiak.spring.aop;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.jaceksysiak.spring.camera.accessories.Lens;
+
+public class App {
+
+	public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/jaceksysiak/spring/aop/beans.xml");
+ 
+        ICamera camera = (ICamera)context.getBean("camera"); 
+        
+        try {
+			camera.snap();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        camera.snap(500);
+        camera.snap(1.8);
+        camera.snap(500, 1.8);
+        camera.snapNighttime();
+        
+        Car car = (Car)context.getBean("car");
+        car.start();
+        
+        camera.snapCar(new Car());
+
+        context.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ spring-tutorial-Aspect-Oriented-Programming-AOP-88/src/com/jaceksysiak/spring/aop/Machine.java
@@ -0,0 +1,5 @@
+package com.jaceksysiak.spring.aop;
+
+public interface Machine {
+
+}
